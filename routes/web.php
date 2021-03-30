a14b29fa42d336b725d16238b4cb1789925bac1f
woq <eveqvq@gmail.com>
2021-03-30 23:38:42 +0800

    try tailwind

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/add', function () {
+    return view('index');
+});
+
+Route::match(['get','post'],'/post', function () {
+    return '这里只接受POST数据';
 });
