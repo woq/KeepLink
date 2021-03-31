d0aa89f9ceae92528929010a7d525883a738d378
woq <eveqvq@gmail.com>
2021-03-31 19:08:29 +0800

    try tailwindcss 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,8 +1,5 @@
-<!doctype html>
 <head>
-    <!-- ... --->
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 </head>
-<!-- ... --->
